745e97d66802f9ec35ccc64c44cb8fa9c833bc07
Wikus Richards <WHOBAY@Gmail.com>
2012-10-12 22:20:03 +0200

    Added the transactions script.

--- org.flying.lions.uitest.MainActivity/assets/www/Newfolder/scripts/showaccounts.js
+++ org.flying.lions.uitest.MainActivity/assets/www/Newfolder/scripts/showaccounts.js
@@ -2,6 +2,17 @@ var Accounts = new Array();
             var newAccounts = new Array();
             var tmpSQL ="";
             //var db;
+            
+            var Bank = new Array();
+            var BankUp = new Array();
+            var accName = new Array();
+            var accNameUp = new Array();
+            /*
+             * 
+             * ABSA cheque account: Balance R1234<br>
+	        	Income: R20000<br>
+	        	Expenses: R1233<br>
+             */
 
             // Query the database
             
@@ -11,8 +22,33 @@ var Accounts = new Array();
                 //tx.executeSql('SELECT * FROM sms', [], querySuccess, errorCB);
             }
             
+            function header(headingTitle)
+            {
+                var tmp = '<li data-role="list-divider" role="heading" class="ui-li ui-li-divider ui-bar-d ui-li-has-count">';
+                tmp += headingTitle;
+                tmp += '<span class="ui-li-count ui-btn-up-c ui-btn-corner-all">2</span>';
+                tmp += '</li>';
+                return tmp;			
+            }
+            
+            function content()
+            {
+                var tmp = '<li data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="div" data-icon="arrow-r" data-iconpos="right" data-theme="d" class="ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-btn-up-d">';
+                tmp += '<div class="ui-btn-inner ui-li">';
+                tmp += '<div class="ui-btn-text">';
+                tmp += '<a href="transactions.html" class="ui-link-inherit">';
+                tmp += '<h3 class="ui-li-heading">ABSA: cheque account</h3>';
+                tmp += '<p class="ui-li-desc"><strong>Category: Other</strong></p>';
+                tmp += '<p class=" ui-li-aside ui-li-desc">R1021.00</p>';	
+                tmp += '</a>';
+                tmp += '</div>';
+                tmp += '<span class="ui-icon ui-icon-arrow-r ui-icon-shadow"></span></div></li>';
+                return tmp;
+            }
+            
             function bank_accountSuccess(tx, results)
             {
+                
                 var len = results.rows.length;
                 //$('<h3>List of Accounts in use</h3>').appendTo('div.data');
 				var ht_str ='<h4>Overview:</h4>';
@@ -20,15 +56,43 @@ var Accounts = new Array();
                 {
                     ht_str +='<h3>No accounts yet</h3>';
                 }
-				
+                //ht_str = header('Wikus');
+		
+//Get Banks		
                 for (var i=0; i<len; i++)
                 {
                     //Accounts.push(results.rows.item(i).Account_Num);
-					console.log(results.rows.item(i).Acc_Name);
-                    ht_str+='<hr>'+results.rows.item(i).Acc_Name+'('+results.rows.item(i).Account_Num+'): Balance '+results.rows.item(i).Balance+'<br>';//.appendTo('div.data');
+                    console.log(results.rows.item(i).Acc_Name);
+                    var tmpBank = results.rows.item(i).Bank;
+                    if(BankUp.indexOf(tmpBank.toUpperCase()) < 0)
+                    {
+                        BankUp.push(tmpBank.toUpperCase());
+                        Bank.push(tmpBank);
+                        ht_str += header(tmpBank);
+                        //ht_str += content();
+                    }
+                    //ht_str+='<hr>'+results.rows.item(i).Acc_Name+'('+results.rows.item(i).Account_Num+'): Balance '+results.rows.item(i).Balance+'<br>';//.appendTo('div.data');
+                }
+                
+//Get Accounts
+                for (i=0; i<len; i++)
+                {
+                    //Accounts.push(results.rows.item(i).Account_Num);
+                    console.log(results.rows.item(i).Acc_Name);
+                    var tmpAcc = results.rows.item(i).Acc_Name;
+                    if(accNameUp.indexOf(tmpAcc.toUpperCase()) < 0)
+                    {
+                        accNameUp.push(tmpAcc.toUpperCase());
+                        accName.push(tmpAcc);
+                        alert(tmpAcc);
+                        ht_str += content();
+                    }
+                    //ht_str+='<hr>'+results.rows.item(i).Acc_Name+'('+results.rows.item(i).Account_Num+'): Balance '+results.rows.item(i).Balance+'<br>';//.appendTo('div.data');
                 }
                 //$("<input class=\"button\" type=\"button\" value=\"Back\" onclick=\"redirect('../Accounts.html');\"/>").appendTo("div.data");
-				$('div#acc').html(ht_str);
+				
+                                
+                               $('ul#acc').html(ht_str);
             }
 
             // Transaction error callback
